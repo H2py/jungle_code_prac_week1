48987e82a17173f7dd61aa1e5d6f9d082293ee8d
H2py <166798061+H2py@users.noreply.github.com>
2025-05-17 11:23:30 +0900

    [Bronze II] Title: OX퀴즈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,19 @@
+import sys
+test = int(sys.stdin.readline())
+
+input_list = []
+for _ in range(test):
+    input = str(sys.stdin.readline())
+    input_list.append(input)
+    
+for str in input_list:
+    sum = 0
+    next = 0
+    for i in str:
+        if i == 'O':
+            sum += 1 + next
+            next += 1
+        elif i == 'X':
+            next = 0
+        else : break
+    print(sum)
