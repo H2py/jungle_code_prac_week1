e3bba30d40dd343e39e2e499c3d641bc2c38a41f
H2py <ahdosj@gmail.com>
2025-05-20 00:44:52 +0900

    z 문제... 푸는 중

--- Z.py
+++ Z.py
@@ -3,18 +3,31 @@ import sys
 
 num, row, col = map(int, input().split())
 total = 0
-lo = (2**num)
+side = (2**num)
 def find_z(n,r,c):
-    global total, lo
-    total += 1
-    if(n == 1): 
-        print(total)
-        return
+    global total, side
+    total+=1
+    if(n == 0): 
+        return 0
     else :
-        n-=1
-        lo /= 2
-        find_z(n,r,c)
-        find_z(n,r,c+lo)
-        find_z(n,r+lo,c)
-        find_z(n,r+lo,c+lo)
-find_z(num, row, col)
+        side //= 2
+        find_z(n-1,r,c)
+        find_z(n-1,r,c+side)
+        find_z(n-1,r+side,c)
+        find_z(n-1,r+side,c+side)
+find_z(0, 0, col)
+print(total)
+
+
+# n=3일 때, 재귀적으로 그려줄 4가지를 다시 출력할 것이다
+# side(변의 길이) = 8 // 2
+# 1.find_z(n-1, r, c)
+# 2.find_z(n-1, r, c+side)
+# 3.find_z(n-1, r + side, c)
+# 4.find_z(n-1, r+side, c+side)
+
+# 다음 호출 시, 
+# find_z(2, r, c)  
+# find_z(2, r, c+4)  
+# find_z(2, r+4, c)  
+# find_z(2, r+4, c+4)  
