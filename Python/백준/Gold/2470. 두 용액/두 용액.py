c285f4390458fd5946713b4094381a18cbafb82c
H2py <166798061+H2py@users.noreply.github.com>
2025-05-24 00:44:15 +0900

    [Gold V] Title: 두 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

---
+++ Python/백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,27 @@
+import sys
+# sys.stdin = open('input.txt', 'r')
+n = int(input())
+liquids = list(map(int, sys.stdin.readline().split()))
+liquids.sort()
+
+start = 0
+end = n - 1
+min_sum = float('inf')
+answer = []
+while start < end:
+    current_sum = liquids[start] + liquids[end]
+    
+    if abs(current_sum) < min_sum:
+        min_sum = abs(current_sum)
+        answer = [liquids[start], liquids[end]]
+    
+    if current_sum == 0:
+        print(f"{answer[0]} {answer[1]}")
+        break
+    elif current_sum < 0:
+        start += 1
+    else :
+        end -= 1
+        
+
+print(f"{answer[0]} {answer[1]}")    
