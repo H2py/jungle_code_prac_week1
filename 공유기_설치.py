8f73baae8e33baa0ff2193432242c3e3f948d1f0
H2py <ahdosj@gmail.com>
2025-05-24 12:55:50 +0900

    배열합치기

--- 공유기_설치.py
+++ 공유기_설치.py
@@ -1,26 +1,25 @@
 import sys
-# sys.stdin = open('input.txt', 'r')
+def binary_search(start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        cnt, now = 1, mid + houses[0]
+        
+        for house in houses:
+            if house >= now:
+                cnt +=1
+                now = house + mid
+        
+        if cnt >= wifi:
+            start = mid + 1
+        else:
+            end = mid - 1
+            
+    return end
             
 h, wifi = map(int, input().split())
 houses = list(map(int, sys.stdin.readlines()))
 houses.sort()
 
-start, end = 1, houses[-1] - houses[0]
-result = 0
-while start <= end:
-    mid = (start + end) // 2        
-    res = []
-    res.append(houses[0])
-    
-    for house in houses:
-        if house - res[-1] >= mid:
-            res.append(house)
-    
-    if len(res) >= wifi:
-        result = mid
-        start = mid+1
-
-    else:
-        end = mid-1
-
-print(result)
+start, end = 1, (houses[-1] - houses[0]) 
+print(binary_search(start, end))
+# 1 2 4 8 9
