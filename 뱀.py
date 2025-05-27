3d06c8c1e004c5b3190a3a0425c145d7066cdc5c
H2py <ahdosj@gmail.com>
2025-05-27 20:23:30 +0900

    SNAKKEKEKEKEEE

--- 뱀.py
+++ 뱀.py
@@ -2,44 +2,43 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
+
 dx = [0, 1, 0, -1]  
 dy = [1, 0, -1, 0]
 
 N = int(input())
 K = int(input())
-
 board = [[0] * N for _ in range(N)]
-shifts = []
 
 for _ in range(K):
-    cord = list(map(int, input().split()))
-    board[cord[0] - 1][cord[1] - 1] = 1
-    
+    r, c = map(int, input().split())
+    board[r-1][c-1] = 1
+
 L = int(input())
+shifts = []
 for _ in range(L):
     t, d = input().split()
     shifts.append((int(t), d))
 
-i = 0
-snake = deque([(0,0)])
-board[0][0] = 2
-direction = 0
-x,y  = 0, 0
 time = 0
+direction = 0
+i = 0
+x, y = 0, 0
+snake = deque([(0, 0)])
+board[0][0] = 2 
 
 while True:
     nx = x + dx[direction]
     ny = y + dy[direction]
     time += 1
-    
-    if nx < 0 or nx >= N or ny < 0 or ny >= N:
+
+    if nx < 0 or ny < 0 or nx >= N or ny >= N:
         break
-    
+
     if board[nx][ny] == 2:
         break
-    
-    
-    if board[nx][ny] == 1:
+
+    if board[nx][ny] == 1:  
         board[nx][ny] = 2
         snake.append((nx, ny))
     else:
@@ -47,14 +46,14 @@ while True:
         snake.append((nx, ny))
         prev_x, prev_y = snake.popleft()
         board[prev_x][prev_y] = 0
-    
+
     x, y = nx, ny
-    
-    if i < len(shifts) and time == shifts[i][0]:
-        if shifts[i][1] == 'D':  
+
+    if i < len(shifts) and shifts[i][0] == time: 
+        if shifts[i][1] == 'D':
             direction = (direction + 1) % 4
-        else:  
+        else:
             direction = (direction - 1) % 4
         i += 1
 
-print(time)    
+print(time)
--- 철로.py
+++ 철로.py
@@ -3,3 +3,30 @@ import heapq
 
 input = sys.stdin.readline
 n = int(input())
+input_list = []
+
+for _ in range(n):
+    h, o = map(int, input().split())
+    if h > o:
+        heapq.heappush(input_list, [o, h])
+    else :
+        heapq.heappush(input_list, [h, o]) 
+        
+input_list.sort(key=lambda x: x[1])  
+
+d = int(input())
+hp = []
+answer = 0
+
+for start, end in input_list:
+    if end - start > d:
+        continue
+    
+    heapq.heappush(hp, start)
+    
+    while hp and hp[0] < end - d:
+        heapq.heappop(hp)
+    
+    answer = max(answer, len(hp))
+
+print(answer)
