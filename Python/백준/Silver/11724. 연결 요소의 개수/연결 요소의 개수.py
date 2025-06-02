2dc3f5381adc00c325049b7a6d1fc0b24d82c6fd
H2py <166798061+H2py@users.noreply.github.com>
2025-06-03 00:39:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 576 ms, Memory: 65956 KB -BaekjoonHub

--- Python/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ Python/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,26 +1,28 @@
 import sys
-sys.setrecursionlimit(10**6)  
-input = sys.stdin.readline    
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
+
+cnt = 0
 graph = [[] for _ in range(N+1)]
 visited = [False] * (N+1)
-cnt = 0
 
 for _ in range(M):
-    a, b = map(int, input().split())
+    a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-
+    
 def dfs(v):
     visited[v] = True
     for i in graph[v]:
         if not visited[i]:
+            visited[i] = True
             dfs(i)
-
+            
 for i in range(1, N+1):
     if not visited[i]:
+        cnt +=1
         dfs(i)
-        cnt += 1
-
+            
 print(cnt)
