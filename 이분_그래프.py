d848b0060bf63d75c68fbb8fed06a87711ac8a69
H2py <ahdosj@gmail.com>
2025-06-03 01:17:26 +0900

    난 이제 지쳤어요 땡벌

---
+++ 이분_그래프.py
@@ -0,0 +1,8 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    
---
+++ 트리의_부모_찾기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+visited = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+
+def dfs(v):    
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = v
+            dfs(i)
+            
+dfs(1)            
+
+for i in range(2, n+1):
+    print(visited[i])
