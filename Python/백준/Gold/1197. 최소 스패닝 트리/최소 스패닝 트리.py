db136fde8154d8b722648c6d87f055a8123fc628
H2py <166798061+H2py@users.noreply.github.com>
2025-06-04 01:39:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 236 ms, Memory: 51144 KB -BaekjoonHub

---
+++ Python/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.rank = [0] * (n + 1)
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        elif self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
+        return True
+
+def kruskal_mst(V, edges):
+    edges.sort(key=lambda x: x[2])
+    uf = UnionFind(V)
+    total_weight = 0
+    
+    for u, v, weight in edges:
+        if uf.union(u, v):
+            total_weight += weight
+    
+    return total_weight
+
+V, E = map(int, input().split())
+edges = []
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edges.append((u, v, w))
+
+print(kruskal_mst(V, edges))
