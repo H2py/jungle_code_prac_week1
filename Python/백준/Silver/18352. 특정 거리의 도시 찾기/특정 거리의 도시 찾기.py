c26d02dfda8e305dad013f2fa721a41d66e29f6f
H2py <166798061+H2py@users.noreply.github.com>
2025-06-04 17:26:49 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1392 ms, Memory: 101148 KB -BaekjoonHub

--- Python/백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ Python/백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -5,35 +5,35 @@ input = sys.stdin.readline
 N,M,K,X = map(int, input().split())
 
 graph = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
-
 for _ in range(M):
     a,b = map(int, input().split())
     graph[a].append(b)
 
-result = []
-def dfs(graph, start, target):
-    global result
+def bfs(graph, start, target):
+    visited = [False] * (N+1)
     visited[start] = True
-    
     queue = deque([(start, 0)])
+    result = []
     
     while queue:
         node, cur_dist = queue.popleft()
         
         if cur_dist == target:
             result.append(node)
+            continue
         
         for neighbor in graph[node]:
             if not visited[neighbor]:
                 visited[neighbor] = True
                 queue.append((neighbor, cur_dist+1))
+                
+    return result
         
                 
-dfs(graph, X, K)
+result = bfs(graph, X, K)
+
 if not result:
     print(-1)
 else:
-    result.sort()
-    for v in result:
-        print(v)
+    for city in sorted(result):
+        print(city)
