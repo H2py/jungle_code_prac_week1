764c8035bffd05a005fca6553eb12daa45932699
H2py <ahdosj@gmail.com>
2025-06-09 20:53:55 +0900

    Honey jam LCS

--- 전깃줄.py
+++ 전깃줄.py
@@ -2,27 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-pole = []
-dp = [0] * 501
-cnt = 0
-
-for _ in range(N):
-    a, b = map(int, input().split())
-    if a > b:
-        pole.append((abs(b-a), b,a))
-    else:
-        pole.append((abs(b-a),a,b))
-
+pole = [tuple(map(int, input().split())) for _ in range(N)]
+pole.sort()
+dp = [1] * N
 
 for i in range(N):
-    for j in range(pole[i][1], pole[i][2]+1):
-        dp[j] += 1
+    for j in range(i):
+        if pole[i][1] > pole[j][1]:
+            dp[i] = max(dp[j] + 1, dp[i])
 
-max_pole = max(pole)
-print(max_pole)
-while max_pole[0] <= 1:
-    cnt += 1
-    for i in range(max_pole[1], max_pole[2]+1):
-        dp[i] -= 1
-    print(dp)
-print(cnt)
+print(len(pole) - max(dp))
