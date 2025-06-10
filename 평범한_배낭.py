b7fc616cffb8c65134fb5af7c5e5124c48e421fa
H2py <ahdosj@gmail.com>
2025-06-10 10:23:07 +0900

    It's not normal knapsack

--- 평범한_배낭.py
+++ 평범한_배낭.py
@@ -3,23 +3,19 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 wv = [tuple(map(int, input().split())) for _ in range(N)]
-dp = [[0] * (K+1) for _ in range(N+1)]
 
-w,v = wv[0]
-for i in range(K+1):
-    if i >= w:
-        dp[0][i] = v
+dp = [[0] * (K + 1) for _ in range(N + 1)]
 
-for i in range(1, N):        
-    weight, value = wv[i]
-    
-    for j in range(K+1):
-        if j < weight:
-            dp[i][j] = dp[i-1][j]
+for i in range(N):
+    w, v = wv[i]
+    for j in range(K + 1):
+        if j < w:
+            dp[i + 1][j] = dp[i][j]
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+            dp[i + 1][j] = max(dp[i][j], dp[i][j - w] + v)
 
-print(dp[N-1][K])
+print(dp[N][K])
+
 
 
 import sys
