410b0466f9a1b3271dea9c2c7bac570d8bfb407c
H2py <166798061+H2py@users.noreply.github.com>
2025-06-11 20:30:02 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+elects = list(map(int, input().split()))
+used = []
+cnt = 0
+
+for i in range(K):
+    cur = elects[i]
+    
+    if cur in used:
+        continue
+        
+    if len(used) < N:
+        used.append(cur)
+        continue
+        
+    far_idx = -1
+    
+    for u in used:
+        if u not in elects[i+1:]:
+            target = u
+            break
+        else:
+            next_idx = elects[i+1:].index(u)
+            if next_idx > far_idx:
+                far_idx = next_idx
+                target = u
+                
+    used.remove(target)
+    used.append(cur)
+    cnt+=1
+
+print(cnt)   
+    
